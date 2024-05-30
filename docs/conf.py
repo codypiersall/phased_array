8d5a8910cfd54cea3fed19064a9f923fcf2c8197
Cody Piersall <cody@ou.edu>
2024-05-29 22:35:53 -0500

    Run black on docs and notebook

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'phased_array'
-copyright = '2024, Cody Piersall'
-author = 'Cody Piersall'
+project = "phased_array"
+copyright = "2024, Cody Piersall"
+author = "Cody Piersall"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -20,13 +20,12 @@ extensions = [
     "sphinxcontrib.katex",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
